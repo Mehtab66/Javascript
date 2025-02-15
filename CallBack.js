bc232d28812c061ba755cc0457743c67b461bbeb
Muhammad Mehtab AHmed <mehtabahmed7777777@gmail.com>
2025-02-15 12:55:45 +0500

    Closure

--- CallBack.js
+++ CallBack.js
@@ -1,5 +1,5 @@
-// Function that takes another function as a argumrnt in called high order function
-
+// Function that takes another function as a argument is called high order function
+// Function which is passed as a argument is called callback function
 function add(a, b, callback) {
   let result = a + b;
   callback(result);
@@ -11,3 +11,13 @@ function ShowResult(result) {
 
 //
 add(5, 5, ShowResult);
+
+//You can even return a fuction from a function
+function addition(a, b, callback) {
+  let result = a + b;
+  callback(result);
+  return () => console.log("Result : " + result);
+}
+
+let ResultFunction = addition(5, 10, () => {});
+ResultFunction();
